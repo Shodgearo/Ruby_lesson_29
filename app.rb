fd4efdc8a2eeac9f3c96d7fe6b8e94acc132f20f
Shodgearo <sanya.93.26@bk.ru>
2019-02-26 14:32:40 +0600

    Edit gem file and fix typo

--- app.rb
+++ app.rb
@@ -3,10 +3,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-set :datebase, "sqlite3:barbershop.db"
+set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-	
 end
 
 get '/' do
